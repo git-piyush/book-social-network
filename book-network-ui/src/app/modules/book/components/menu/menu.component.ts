6afe91588acd4ccc25c7ac8e4bfd1d070dea5a27
Piyush kumar <kmrpiyush95@gmail.com.com>
2024-07-10 15:17:36 +0530

    first commit

---
+++ book-network-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {KeycloakService} from '../../../../services/keycloak/keycloak.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+userName: any;
+
+  constructor(
+    private keycloakService: KeycloakService
+  ) {
+  }
+    ngOnInit(): void {
+      const linkColor = document.querySelectorAll('.nav-link');
+      linkColor.forEach(link => {
+        if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+          link.classList.add('active');
+        }
+        link.addEventListener('click', () => {
+          linkColor.forEach(l => l.classList.remove('active'));
+          link.classList.add('active');
+        });
+      });
+      this.userName = localStorage.getItem('user');
+    }
+
+  async logout() {
+    await this.keycloakService.logout();
+  }
+}
